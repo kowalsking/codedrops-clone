54d98a376b948c9dd6a2294f3684808927eee2c3
kowalsking <kovvalsking@gmail.com>
2022-05-13 17:12:06 +0300

    Skeleton of main content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navigation from './components/navigation'
+import Main from './components/main'
 
 function App() {
   return (
     <div className="container">
       <Navigation />
+      <Main />
     </div>
   )
 }
---
+++ src/components/main/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="sidebar">
+        <div className="subscribe">
+          Subscribe
+        </div>
+        <div className="latest">
+          Latest
+        </div>
+        <div className="css-reference">
+          Css reference
+        </div>
+      </div>
+      <div className="content">
+        Content
+      </div>
+      <div className="empty-container"></div>
+    </div>
+  )
+}
+
+export default Main;
